f6befa3d24ace2880c1f5da9f87a2e669280d298
noogoolgga <rkdguddn21@gmail.com>
2026-04-02 12:42:29 +0900

    콜백타입 추가
    방 입장 콜백타입 추가

--- IOCPChatServer/StrandCallback.h
+++ IOCPChatServer/StrandCallback.h
@@ -5,6 +5,7 @@ enum class StrandCallbackType
 {
 	FREE_USER,			//	DISCONNECT -> UserManager에서 제거
 	USER_LEFT_ROOM,		//	ROOM_LEAVE -> DomainState를 Login으로 복원
+	 USER_ENTERED_ROOM,	//	ROOM_ENTER -> DomainState를 Room으로 변경
 	// 추후 확장
 };
 
@@ -12,6 +13,8 @@ struct StrandCallback
 {
 	StrandCallbackType type;
 	uint32_t clientIndex;
+	int32_t roomNumber = -1;	// 입장한 방 번호 전달용
+	uint16_t result = 0;		// 방 입장 결과 코드 전달용
 
 	// MPSC Queue 링크용
 	std::atomic<StrandCallback*> mpscNext{ nullptr };
